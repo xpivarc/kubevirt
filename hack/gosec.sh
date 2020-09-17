d442033a99870d19e8bbbefbc1142583710ee3c3
L. Pivarc <lpivarc@redhat.com>
2020-09-17 16:02:44 +0200

    ouput juint
    Signed-off-by: L. Pivarc <lpivarc@redhat.com>

--- hack/gosec.sh
+++ hack/gosec.sh
@@ -1,6 +1,9 @@
 source hack/common.sh
 
+export ARTIFACTS=$KUBEVIRT_DIR/${ARTIFACTS:-_out/artifacts}
+mkdir -p $ARTIFACTS
 
 echo "Run go sec in pkg"
 cd $KUBEVIRT_DIR/pkg
-gosec -exclude-dir=testutils -fmt=junit-xml -out=output.xml ./... 
+set -x
+gosec -exclude-dir=testutils -fmt=junit-xml -out=junit-gosec.xml ./... 
