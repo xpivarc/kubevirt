3e402ea2f03afd4c5019c50a6e25dce92ce0b008
L. Pivarc <lpivarc@redhat.com>
2021-02-19 13:52:37 +0100

    (MacvTap) Change ownership of tap device
    Signed-off-by: L. Pivarc <lpivarc@redhat.com>

--- pkg/virt-handler/vm.go
+++ pkg/virt-handler/vm.go
@@ -2441,7 +2441,9 @@ func (d *VirtualMachineController) vmUpdateHelperMigrationTarget(origVMI *v1.Vir
 	}
 
 	if virtutil.IsNonRootVMI(vmi) {
-		d.prepareStorage(vmi, origVMI)
+		if err := d.nonRootSetup(origVMI, vmi); err != nil {
+			return err
+		}
 	}
 
 	if err := client.SyncMigrationTarget(vmi); err != nil {
@@ -2519,7 +2521,9 @@ func (d *VirtualMachineController) vmUpdateHelperDefault(origVMI *v1.VirtualMach
 
 		}
 		if virtutil.IsNonRootVMI(vmi) {
-			d.prepareStorage(vmi, origVMI)
+			if err := d.nonRootSetup(origVMI, vmi); err != nil {
+				return err
+			}
 		}
 
 		// set runtime limits as needed
