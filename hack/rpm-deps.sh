c67ce66fd2eff91e906a637ddee5b07bf5e308c9
L. Pivarc <lpivarc@redhat.com>
2021-02-22 17:05:49 +0100

    Libvirt update

--- hack/rpm-deps.sh
+++ hack/rpm-deps.sh
@@ -5,9 +5,9 @@ set -ex
 source hack/common.sh
 source hack/config.sh
 
-LIBVIRT_VERSION=0:6.6.0-13
+LIBVIRT_VERSION=0:7.0.0-2
 SEABIOS_VERSION=0:1.14.0-1
-QEMU_VERSION=15:5.1.0-18
+QEMU_VERSION=2:5.2.0-4
 
 # Define some base packages to avoid dependency flipping
 # since some dependencies can be satisfied by multiple packages
