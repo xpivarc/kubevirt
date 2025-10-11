41d59f2427f161f2e368449bed9fa3ba4f33d70f
Luboslav Pivarc <lpivarc@redhat.com>
2025-10-11 19:37:20 +0200

    Old
    cpu: Intel(R) Core(TM) i7-10850H CPU @ 2.70GHz
    BenchmarkListMatchingTargetPods1000-12     	    9240	    114762 ns/op	   17799 B/op	      27 allocs/op
    BenchmarkListMatchingTargetPods10000-12    	     763	   1477305 ns/op	  165239 B/op	      27 allocs/op

---
+++ pkg/virt-controller/watch/migration/migration_bench_test.go
@@ -0,0 +1,110 @@
+package migration
+
+import (
+	"fmt"
+	"testing"
+
+	k8sv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	v1 "kubevirt.io/api/core/v1"
+
+	"kubevirt.io/kubevirt/pkg/testutils"
+)
+
+func setup(podCount int) (*Controller, *v1.VirtualMachineInstance, *v1.VirtualMachineInstanceMigration, *k8sv1.Pod) {
+	informer, _ := testutils.NewFakeInformerFor(&k8sv1.Pod{})
+	controller := Controller{
+		podIndexer: informer.GetIndexer(),
+	}
+
+	migration := v1.VirtualMachineInstanceMigration{
+		ObjectMeta: metav1.ObjectMeta{
+			UID:       types.UID("something"),
+			Namespace: "vm-debug",
+		},
+	}
+
+	vmi := v1.VirtualMachineInstance{
+		ObjectMeta: metav1.ObjectMeta{
+			UID:       types.UID("something"),
+			Namespace: "vm-debug",
+		},
+	}
+
+	for i := range podCount - 1 {
+		pod := k8sv1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				UID:       types.UID(fmt.Sprintf("something%d", i)),
+				Namespace: "vm-debug",
+				Name:      fmt.Sprintf("name%d", i),
+				Labels: map[string]string{
+					"kubevirt.io":                         "virt-launcher",
+					"kubevirt.io/created-by":              "b0c482c0-0e0d-40b9-8dff-de967de41e5a",
+					"kubevirt.io/domain":                  "rhel9",
+					"kubevirt.io/migrationJobUID":         "815e781c-44f6-4ae7-9403-dc156cebd1fd",
+					"kubevirt.io/migrationTargetNodeName": "e28-h03-000-r650",
+					"kubevirt.io/nodeName":                "e28-h03-000-r650",
+					"vm.kubevirt.io/name":                 "rhel9-2236",
+				},
+			},
+		}
+		err := controller.podIndexer.Add(&pod)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	pod := k8sv1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			UID:       types.UID("something"),
+			Namespace: "vm-debug",
+			Name:      "name",
+			Labels: map[string]string{
+				"kubevirt.io":                         "virt-launcher",
+				"kubevirt.io/created-by":              string(vmi.UID),
+				"kubevirt.io/domain":                  "rhel9",
+				"kubevirt.io/migrationJobUID":         string(migration.UID),
+				"kubevirt.io/migrationTargetNodeName": "e28-h03-000-r650",
+				"kubevirt.io/nodeName":                "e28-h03-000-r650",
+				"vm.kubevirt.io/name":                 "rhel9-2236",
+			},
+		},
+	}
+
+	err := controller.podIndexer.Add(&pod)
+	if err != nil {
+		panic(err)
+	}
+
+	return &controller, &vmi, &migration, &pod
+
+}
+func BenchmarkListMatchingTargetPods1000(b *testing.B) {
+	benchmarkListMatchingTargetPods(b, 1000)
+}
+
+func BenchmarkListMatchingTargetPods10000(b *testing.B) {
+	benchmarkListMatchingTargetPods(b, 10000)
+}
+
+func benchmarkListMatchingTargetPods(b *testing.B, count int) {
+	controller, vmi, migration, pod := setup(count)
+
+	b.ResetTimer()
+
+	for b.Loop() {
+		pods, err := controller.listMatchingTargetPods(migration, vmi)
+		if err != nil {
+			panic(err)
+		}
+		if len(pods) != 1 {
+			panic("hi")
+
+		}
+		if pods[0] != pod {
+			panic("hi")
+		}
+	}
+
+}
